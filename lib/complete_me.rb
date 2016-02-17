8967abfd85fe4808ba1d1b30da03535af0c867d6
Jonathan Bernesser <jbern16@gmail.com>
2016-02-17 08:31:38 -0700

    insert test almost there

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'node'
 
 class CompleteMe
@@ -14,14 +15,14 @@ class CompleteMe
     unless current_node.children.include?(word[n])
       current_node.children[word[n]] = Node.new
       flag_word(word, n, current_node)
-      insert_word(word, n+1, current_node.children[word[n]]) if n < word.length -1
+      insert_word(word, n+1, current_node.children[word[n]]) if n < word.length - 1
     else
       insert_word(word, n+1, current_node.children[word[n]]) if n < word.length - 1
     end
   end
 
   def flag_word(word, n, current_node)
-    if word[n] == word[-1]
+    if n == word.length - 1
       current_node.children[word[n]].value = true
       @word_count += 1
     end
@@ -31,5 +32,5 @@ class CompleteMe
     @word_count
   end
 
-  
+
 end
