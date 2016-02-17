43aaf1fe660c4a5633ca1bb5a2746b4dc39603ab
Jonathan Bernesser <jbern16@gmail.com>
2016-02-16 21:15:22 -0700

    added count functionality

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -6,6 +6,7 @@ class CompleteMe
 
   def initialize
     @root= Node.new
+    @word_count = 0
   end
 
   def insert_word(word, n=0,current_node=@root)
@@ -22,6 +23,13 @@ class CompleteMe
   def flag_word(word, n, current_node)
     if word[n] == word[-1]
       current_node.children[word[n]].value = true
+      @word_count += 1
     end
   end
+
+  def count
+    @word_count
+  end
+
+  
 end
